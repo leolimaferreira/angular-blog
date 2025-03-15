82764d0dd1bf12820f991aa3c5490787d28571ea
leolimaferreira <leo2342005@gmail.com>
2025-03-15 10:47:11 -0300

    Finalizando o projeto, colocando dinamicamente os conteudos das paginas content.

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
+import {ActivatedRoute} from '@angular/router';
+import {dataFake} from '../data/dataFake';
+import {identity} from 'rxjs';
 
 @Component({
   selector: 'app-content',
@@ -8,9 +11,32 @@ import {Component, OnInit} from '@angular/core';
 })
 export class ContentComponent implements OnInit{
 
-  constructor() {
+  @Input()
+  photoCover:string = "";
+  @Input()
+  contentTitle:string = "";
+  @Input()
+  contentDescription:string = "";
+  private id:string | null= "0";
+
+  constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit() {
+    this.route.paramMap.subscribe(value =>
+      this.id = value.get("id")
+    )
+
+    this.setValuesToComponent(this.id);
+  }
+
+  setValuesToComponent(id: string | null) {
+    const result = dataFake.filter(article =>
+                                                  article.id === id)[0];
+
+    this.photoCover = result.photoCover;
+    this.contentTitle = result.title;
+    this.contentDescription = result.description;
+
   }
 }
---
+++ src/app/pages/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "Playboi Carti lança novo albúm",
+    "description": "syah",
+    "photoCover": "https://www.billboard.com/wp-content/uploads/2024/08/cover-playboi-carti-billboard-2024-bb13-matthew-salacuse-6-1548.jpg"
+  },
+  {
+    "id": "2",
+    "title": "Frank Ocean continua desaparecido",
+    "description": "Oda",
+    "photoCover": "https://i.pinimg.com/236x/9e/35/48/9e3548ac410bc0c5e1631677ab841fa4.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Não tem jeito",
+    "description": "Kendrick Lamar",
+    "photoCover": "https://img.band.uol.com.br/image/2025/02/03/kendrick-lamar-19517_800x450.webp"
+  },
+  {
+    "id": "4",
+    "title": "Drake chorando",
+    "description": "OVHOE",
+    "photoCover": "https://media.pitchfork.com/photos/5f360c09f6eb11025fede4a9/16:9/w_1408,h_792,c_limit/drake.jpg"
+  }
+]
